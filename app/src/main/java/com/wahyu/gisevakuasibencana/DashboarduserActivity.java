aa5bf68b504d2c4482c5885d20e60e670fcd0f29
wahyubudiman30 <wahyuu3004@gmail.com>
2022-12-09 17:32:49 +0800

    9Des

--- app/src/main/java/com/wahyu/gisevakuasibencana/DashboarduserActivity.java
+++ app/src/main/java/com/wahyu/gisevakuasibencana/DashboarduserActivity.java
@@ -1,14 +1,44 @@
 package com.wahyu.gisevakuasibencana;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboarduserActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment f = null;
+            switch (item.getItemId()){
+                case R.id.menu_posko:
+                    f = new FragmentPosko();
+                    break;
+                case R.id.menu_evakuasi:
+                    f = new FragmentPeta();
+                    break;
+                case R.id.menu_bantuan:
+                    f = new FragmentInformasi();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.conteiner_fragment,f).commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboarduser);
+
+
+        bottomNavigationView =findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
     }
 }
